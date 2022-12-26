cba95f985eb62d8d35fe92136e0468b36dcba63c
Kieran Hulsman <kghulsma@uwaterloo.ca>
2022-12-26 00:14:51 -0500

    added factorial function

--- tutorial2/calc.py
+++ tutorial2/calc.py
@@ -1,11 +1,23 @@
-def add (x, y):
+def add(x, y):
     return x + y
 
-def subtract (x, y):
+def subtract(x, y):
     return x - y
 
-def multiply (x, y):
+def multiply(x, y):
     return x * y
 
-def divide (x, y):
+def divide(x, y):
     return x / y
+
+def factorial(x):
+    res = 1
+    for i in range(1, x+1):
+        res *= i
+    return res
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
